fff39d226cd2d1faacc44d452addbf88a3534e50
Dzevad Delihodzic <dzevad.del@gmail.com>
2023-09-24 00:35:10 +0200

    fix: client clienup, move typescript to dev dependencie in root package.json

--- packages/client/app/page.tsx
+++ packages/client/app/page.tsx
@@ -1,24 +1,5 @@
-import React from 'react';
-
 const Home = () => {
-  const test = {
-    one: 1,
-    two: 2,
-  };
-
-  return (
-    <main>
-      <Btn className='mx-auto bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3'>
-        hello text...
-      </Btn>
-    </main>
-  );
+  return <main>test</main>;
 };
 
 export default Home;
-
-const Btn: React.FC<BtnProps> = ({ children, ...rest }) => {
-  return <button {...rest}>{children}</button>;
-};
-
-type BtnProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
