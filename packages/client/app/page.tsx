299a7c4c04d8686911e3e5f2e0eed3b2298aafb4
Dzevad Delihodzic <dzevad.del@gmail.com>
2023-09-23 22:14:03 +0200

    feat: initialize tut-factory app

---
+++ packages/client/app/page.tsx
@@ -0,0 +1,9 @@
+import Button from '@client/components/button/Button';
+
+export default function Home() {
+  return (
+    <main>
+      <Button />
+    </main>
+  );
+}
---
+++ packages/client/components/button/Button.tsx
@@ -0,0 +1,5 @@
+const Button: React.FC<unknown> = () => {
+  return <h1 className='text-[1111px]'>Button</h1>;
+};
+
+export default Button;
