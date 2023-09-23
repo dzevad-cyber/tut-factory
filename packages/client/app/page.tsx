e20d920799a3095945d3251ef01d1b4b22d0e242
Dzevad Delihodzic <dzevad.del@gmail.com>
2023-09-23 22:19:54 +0200

    fix: remo unused components, update gitignore

--- packages/client/app/page.tsx
+++ packages/client/app/page.tsx
@@ -1,9 +1,11 @@
 import Button from '@client/components/button/Button';
+import ButtonXL from '@client/components/buttonXL/ButtonXL';
 
 export default function Home() {
   return (
     <main>
       <Button />
+      <ButtonXL />
     </main>
   );
 }
--- packages/client/components/button/Button.tsx
+++
@@ -1,5 +0,0 @@
-const Button: React.FC<unknown> = () => {
-  return <h1 className='text-[1111px]'>Button</h1>;
-};
-
-export default Button;
