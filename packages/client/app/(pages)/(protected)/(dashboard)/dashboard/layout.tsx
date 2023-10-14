557de6c1ee22d1bc3cceb67e88b20d6ef34da663
Dzevad Delihodzic <dzevad.del@gmail.com>
2023-10-14 12:05:11 +0200

    refactor: dashboard layout, cleanup

--- packages/client/app/(pages)/(protected)/(dashboard)/dashboard/layout.tsx
+++ packages/client/app/(pages)/(protected)/(dashboard)/dashboard/layout.tsx
@@ -17,15 +17,3 @@ export default DashboardLayout;
 type DashboardLayout = {
   children: React.ReactNode;
 };
-
-const Content: React.FC<ContentProps> = ({ children }) => {
-  return (
-    <main className='bg-green-300 md:col-start-2 md:col-end-3 md:row-start-2'>
-      {children}
-    </main>
-  );
-};
-
-type ContentProps = {
-  children: React.ReactNode;
-};
