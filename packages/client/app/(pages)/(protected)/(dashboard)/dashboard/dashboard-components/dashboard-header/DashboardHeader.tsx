c24b23e05800f76f19367e8e24e35a55b304a11f
Dzevad Delihodzic <dzevad.del@gmail.com>
2023-10-15 18:49:55 +0200

    feat: add user button to dashboard header

--- packages/client/app/(pages)/(protected)/(dashboard)/dashboard/dashboard-components/dashboard-header/DashboardHeader.tsx
+++ packages/client/app/(pages)/(protected)/(dashboard)/dashboard/dashboard-components/dashboard-header/DashboardHeader.tsx
@@ -1,8 +1,8 @@
 import { Sheet, SheetContent, SheetTrigger } from '@client/components/ui/sheet';
 import { Menu } from 'lucide-react';
 import DashboardSidebar from '../dashboard-sidebar/DashboardSidebar';
-import { Button } from '@client/components/ui/button';
 import { cn } from '@client/client-utils/tailwind/cn';
+import { UserButton } from '@clerk/nextjs';
 
 const DashboardHeader = () => {
   return (
@@ -28,10 +28,7 @@ const DashboardHeader = () => {
 
       {/* nav menu */}
       <div className={cn('grid', 'grid-flow-col', 'justify-self-end')}>
-        <Button>TEST</Button>
-        <Button>TEST</Button>
-        <Button>TEST</Button>
-        <Button>TEST</Button>
+        <UserButton />
       </div>
     </div>
   );
