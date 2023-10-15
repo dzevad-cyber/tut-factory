50748e3d8aa8220dbb00c3667447a8d827c2e5c0
Dzevad Delihodzic <dzevad.del@gmail.com>
2023-10-15 19:21:50 +0200

    fix: update clerk button styles

--- packages/client/app/(pages)/(protected)/(dashboard)/dashboard/dashboard-components/dashboard-header/DashboardHeader.tsx
+++ packages/client/app/(pages)/(protected)/(dashboard)/dashboard/dashboard-components/dashboard-header/DashboardHeader.tsx
@@ -28,7 +28,33 @@ const DashboardHeader = () => {
 
       {/* nav menu */}
       <div className={cn('grid', 'grid-flow-col', 'justify-self-end')}>
-        <UserButton afterSignOutUrl='/' />
+        <UserButton
+          afterSignOutUrl='/'
+          appearance={{
+            elements: {
+              avatarBox: {
+                width: '4rem',
+                height: '4rem',
+              },
+              userPreviewMainIdentifier: {
+                fontSize: '1.6rem',
+              },
+              userPreviewSecondaryIdentifier: {
+                fontSize: '1.1rem',
+                color: 'hsl(20, 14.3%, 4.1%)',
+              },
+              userButtonPopoverActionButtonIcon: {
+                width: '1.2rem',
+                height: '1.2rem',
+                color: 'hsl(20, 14.3%, 4.1%)',
+              },
+              userButtonPopoverActionButtonText: {
+                fontSize: '1.2rem',
+                color: 'hsl(20, 14.3%, 4.1%)',
+              },
+            },
+          }}
+        />
       </div>
     </div>
   );
