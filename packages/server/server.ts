20c5d211bf153b9897ff353cf0112df348ad09d8
Dzevad Delihodzic <dzevad.del@gmail.com>
2023-10-20 00:44:34 +0200

    feat: add swc for 10x performance server boot time

--- packages/server/server.ts
+++ packages/server/server.ts
@@ -1,4 +1,8 @@
+console.time('Server startup time');
 import app from './app/app';
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+  console.timeEnd('Server startup time');
+});
