d3316e8cc7cca7c0815494be6a98943ecbc7ebc9
Dzevad Delihodzic <dzevad.del@gmail.com>
2023-10-26 00:53:07 +0200

    style: fix toggle theme button styles

--- packages/client/components/client-components/toggle-theme/ToggleTheme.tsx
+++ packages/client/components/client-components/toggle-theme/ToggleTheme.tsx
@@ -18,7 +18,7 @@ const ToggleTheme = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant='outline' size='sm' className={cn('p-5')}>
+        <Button variant='outline' size='lg' className={cn('w-[100%]')}>
           <Sun className='h-[2rem] w-[2rem] rotate-0 scale-100  transition-all dark:-rotate-90 dark:scale-0' />
           <Moon className='absolute h-[2rem] w-[2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
           <span className='sr-only'>Toggle theme</span>
