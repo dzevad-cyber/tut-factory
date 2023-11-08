5443badc5d4d08655ca27bb7e9a4c167e98498c7
Dzevad Delihodzic <dzevad.del@gmail.com>
2023-11-08 12:05:09 +0100

    feat: update user model schema attributes
    fix: add attributes and update user model

--- packages/server/src/app/models/user.model.ts
+++ packages/server/src/app/models/user.model.ts
@@ -6,20 +6,66 @@ const User = sequelize.define(
   {
     first_name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
+      validate: {
+        isAlpha: {
+          msg: 'First name must only contain letters',
+        },
+        len: {
+          args: [3, 30],
+          msg: 'First name must be between 2 and 30 characters',
+        },
+      },
     },
     last_name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
+      validate: {
+        isAlpha: {
+          msg: 'Last name name must only contain letters',
+        },
+        len: {
+          args: [3, 30],
+          msg: 'Last name must be between 2 and 30 characters',
+        },
+      },
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
+      validate: {
+        isEmail: {
+          msg: 'Invalid email address',
+        },
+      },
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        len: {
+          args: [12, 30],
+          msg: 'Password must be between 12 and 30 characters',
+        },
+      },
+    },
+    avatar: {
+      type: DataTypes.STRING,
+    },
+    role: {
+      type: DataTypes.STRING,
+      defaultValue: 'user',
+      validate: {
+        isIn: {
+          args: [['user', 'admin', 'moderator', 'teacher']],
+          msg: 'Role must be either user, admin, moderator or teacher',
+        },
+      },
+    },
+    isEmailVerified: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
   },
   {
