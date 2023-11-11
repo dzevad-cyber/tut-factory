70a1314112c18cb82c1e6cd8662aa10aeba77ef6
Dzevad Delihodzic <dzevad.del@gmail.com>
2023-11-11 11:10:22 +0100

    fieat: create auth controller, register controller, refactor catchAsync export to named

--- packages/server/src/app/controllers/user/user.controller.ts
+++ packages/server/src/app/controllers/user/user.controller.ts
@@ -1,11 +1,9 @@
 import User from '../../models/user.model';
-import catchAsync from '../../utils/catchAsync';
+import { catchAsync } from '../../utils/catchAsync';
+import { responseSuccess } from '../../utils/reponses/reponses';
 
 export const createUser = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
 
-  res.status(200).json({
-    status: 'success',
-    user: newUser,
-  });
+  return responseSuccess(res, 201, newUser);
 });
