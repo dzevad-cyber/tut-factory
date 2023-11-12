b8a9b020b1ba6713c33d8f66ba56917a42802155
Dzevad Delihodzic <dzevad.del@gmail.com>
2023-11-13 00:34:59 +0100

    refactor: wrap register  middleware in catchAsync

--- packages/server/src/app/middlewares/register/register.middleware.ts
+++ packages/server/src/app/middlewares/register/register.middleware.ts
@@ -1,36 +1,35 @@
 import { NextFunction, Request, Response } from 'express';
 import z from 'zod';
 import { responseFail } from '../../utils/reponses/reponses';
+import { catchAsync } from '../../utils/catchAsync';
 
-export const validateRegisterReqBody = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const reqBodySchema = z
-    .object({
-      firstName: z.string().min(3).max(30),
-      email: z.string().email(),
-      password: z.string().min(12).max(30),
-      confirmPassword: z.string().min(12).max(30),
-    })
-    .refine((data) => data.password === data.confirmPassword, {
-      message: 'Passwords do not match',
-      path: ['confirmPassword'],
-    });
+export const validateRegisterReqBody = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const reqBodySchema = z
+      .object({
+        firstName: z.string().min(3).max(30),
+        email: z.string().email(),
+        password: z.string().min(12).max(30),
+        confirmPassword: z.string().min(12).max(30),
+      })
+      .refine((data) => data.password === data.confirmPassword, {
+        message: 'Passwords do not match',
+        path: ['confirmPassword'],
+      });
 
-  const reqBodySchemaParsed = reqBodySchema.safeParse(req.body);
+    const reqBodySchemaParsed = reqBodySchema.safeParse(req.body);
 
-  if (!reqBodySchemaParsed.success) {
-    const errors = reqBodySchemaParsed.error.issues.reduce((acc, curr) => {
-      return {
-        ...acc,
-        [curr.path[0] as string]: curr.message,
-      };
-    }, {});
+    if (!reqBodySchemaParsed.success) {
+      const errors = reqBodySchemaParsed.error.issues.reduce((acc, curr) => {
+        return {
+          ...acc,
+          [curr.path[0] as string]: curr.message,
+        };
+      }, {});
 
-    return responseFail(res, 400, errors);
-  } else {
-    next();
+      return responseFail(res, 400, errors);
+    } else {
+      next();
+    }
   }
-};
+);
