c9bb73d7af23d1e5c6edf8199026aee4520f33a2
Dzevad Delihodzic <dzevad.del@gmail.com>
2023-11-13 00:17:21 +0100

    feat: add password match check for register route middleware

--- packages/server/src/app/middlewares/register/register.middleware.ts
+++ packages/server/src/app/middlewares/register/register.middleware.ts
@@ -7,12 +7,17 @@ export const validateRegisterReqBody = async (
   res: Response,
   next: NextFunction
 ) => {
-  const reqBodySchema = z.object({
-    firstName: z.string().min(3).max(30),
-    email: z.string().email(),
-    password: z.string().min(12).max(30),
-    confirmPassword: z.string().min(12).max(30),
-  });
+  const reqBodySchema = z
+    .object({
+      firstName: z.string().min(3).max(30),
+      email: z.string().email(),
+      password: z.string().min(12).max(30),
+      confirmPassword: z.string().min(12).max(30),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: 'Passwords do not match',
+      path: ['confirmPassword'],
+    });
 
   const reqBodySchemaParsed = reqBodySchema.safeParse(req.body);
 
