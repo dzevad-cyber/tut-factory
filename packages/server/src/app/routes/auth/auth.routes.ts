6531a67d668822a469f8ee56ae462e9cc97b5b21
Dzevad Delihodzic <dzevad.del@gmail.com>
2023-11-13 00:04:55 +0100

    feat: add middleware for validatig register request body

--- packages/server/src/app/routes/auth/auth.routes.ts
+++ packages/server/src/app/routes/auth/auth.routes.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 import * as authController from '../../controllers/auth/auth.controller';
+import { validateRegisterReqBody } from '../../middlewares/register/register.middleware';
 
 export const authRouter = express.Router({ mergeParams: true });
 
-authRouter.post('/auth/register', authController.register);
+authRouter.post(
+  '/auth/register',
+  [validateRegisterReqBody],
+  authController.register
+);
