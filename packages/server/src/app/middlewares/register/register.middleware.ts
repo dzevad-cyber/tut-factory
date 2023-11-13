b6e62eac9bbb72603e0ae5e88f7d7993381d17eb
Dzevad Delihodzic <dzevad.del@gmail.com>
2023-11-14 00:34:20 +0100

    feat: add zod with react-hook from for register

--- packages/server/src/app/middlewares/register/register.middleware.ts
+++ packages/server/src/app/middlewares/register/register.middleware.ts
@@ -1,23 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
-import z from 'zod';
 import { responseFail } from '../../utils/reponses/reponses';
 import { catchAsync } from '../../utils/catchAsync';
+import { registerFormSchema } from '../../../../../shared/src/forms/registerForm';
 
 export const validateRegisterReqBody = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const reqBodySchema = z
-      .object({
-        firstName: z.string().min(3).max(30),
-        email: z.string().email(),
-        password: z.string().min(12).max(30),
-        confirmPassword: z.string().min(12).max(30),
-      })
-      .refine((data) => data.password === data.confirmPassword, {
-        message: 'Passwords do not match',
-        path: ['confirmPassword'],
-      });
-
-    const reqBodySchemaParsed = reqBodySchema.safeParse(req.body);
+    const reqBodySchemaParsed = registerFormSchema.safeParse(req.body);
 
     if (!reqBodySchemaParsed.success) {
       const errors = reqBodySchemaParsed.error.issues.reduce((acc, curr) => {
--- packages/shared/src/types/models/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './course.type';
-export * from './user.type';
