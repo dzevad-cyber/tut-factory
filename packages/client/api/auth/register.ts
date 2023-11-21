4e3ee44efaf6bc507cb601210196cfb820dce20d
Dzevad Delihodzic <dzevad.del@gmail.com>
2023-11-22 00:07:43 +0100

    feat: create custom axios, add baseUrl

--- packages/client/api/auth/register.ts
+++ packages/client/api/auth/register.ts
@@ -1,12 +1,13 @@
+import clientHttp from '@client/clientHttp';
 import { type RegisterForm } from '@shared/forms/register-form/registerForm.types';
 import { useMutation } from '@tanstack/react-query';
-import axios, { AxiosError } from 'axios';
+import { AxiosError } from 'axios';
 
 export const useRegister = () => {
   return useMutation({
     mutationFn: async (formData: RegisterForm) => {
-      const { data }: { data: RegisterForm } = await axios.post(
-        'http://localhost:5000/api/v1/auth/register',
+      const { data }: { data: RegisterForm } = await clientHttp.post(
+        '/api/v1/auth/register',
         formData
       );
       return data;
