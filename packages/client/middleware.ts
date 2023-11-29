b7cde003316c2c48d7a3b6f384dfdeade0ca77be
Dzevad Delihodzic <dzevad.del@gmail.com>
2023-11-29 23:29:56 +0100

    feat: add middleware in nextjs to protect /dashboard pages

--- packages/client/middleware.ts
+++ packages/client/middleware.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(req: NextRequest, res: NextResponse) {
-  console.log('[ middleware.ts - 5 ] - req.cookies:', req.cookies);
-  console.log('test');
+export function middleware(req: NextRequest) {
+  if (
+    req.nextUrl.pathname.startsWith('/dashboard') &&
+    !req.cookies.has('__auth_tf__')
+  ) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
 }
-
-export const config = {
-  matcher: ['/dashboard/:path*', '/teacher/:path*'],
-};
