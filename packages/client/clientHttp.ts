c44332209b29fb79a18172937323842b88577167
Dzevad Delihodzic <dzevad.del@gmail.com>
2023-11-29 20:51:55 +0100

    feat: add withCredentials to axios config

--- packages/client/clientHttp.ts
+++ packages/client/clientHttp.ts
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 const clientHttp = axios.create({
   baseURL: 'http://localhost:5000/api',
+  withCredentials: true,
 });
 
 clientHttp.interceptors.response.use((res) => {
   console.log('[ clientHttp.ts - 11 ] - response:', res);
 
-  
   return res;
 });
 
