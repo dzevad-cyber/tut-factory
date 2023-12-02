08cd69bf269fcfe91446160b13e30e98b971f793
Dzevad Delihodzic <dzevad.del@gmail.com>
2023-12-02 10:54:52 +0100

    refactor: public pages layout

--- packages/client/app/(pages)/(public)/layout.tsx
+++ packages/client/app/(pages)/(public)/layout.tsx
@@ -4,31 +4,34 @@ import NavLink from '@client/components/client-link/NavLink';
 const PublicPagesLayout: React.FC<PublicPagesLayoutProps> = ({ children }) => {
   return (
     <div className={cn('grid', 'min-h-screen', 'grid-rows-[min-content_1fr]')}>
-      <div className={cn('h-24', 'justify-end', 'grid', 'px-10')}>
-        <header>
-          <nav>
-            <ul
-              className={cn(
-                'grid',
-                'grid-flow-col',
-                'gap-x-10',
-                'items-center'
-              )}
-            >
-              <li>
-                <NavLink href='/dashboard'>Dashboard</NavLink>
-              </li>
-              <li>
-                <NavLink href='/register'>Register</NavLink>
-              </li>
-              <li>
-                <NavLink href='/login'>Log in</NavLink>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        {children}
-      </div>
+      <header className={cn('h-24', 'justify-end', 'grid', 'px-10')}>
+        <nav>
+          <ul
+            className={cn(
+              'grid',
+              'grid-flow-col',
+              'gap-x-10',
+              'items-center',
+              'p-5'
+            )}
+          >
+            <li>
+              <NavLink variant='ghost' href='/dashboard'>
+                Dashboard
+              </NavLink>
+            </li>
+            <li>
+              <NavLink variant='ghost' href='/register'>
+                Register
+              </NavLink>
+            </li>
+            <li>
+              <NavLink href='/login'>Log in</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      {children}
     </div>
   );
 };
